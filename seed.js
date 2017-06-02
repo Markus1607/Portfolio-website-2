d52cdf6f6061edea6d3a86e8288aa4d9320a1316
Mark Sarpong <markus20@outlook.com>
2017-06-02 08:34:16 +0100

    Formatted the blog index

--- seed.js
+++ seed.js
@@ -6,19 +6,19 @@ var mongoose = require("mongoose"),
 
 var data = [
   {
-    title: "This is the first blog",
-    details: "this is the first blog i will ever make in my life, this is so awesome i don't think i am gonna stop making it"
+    title: "New features in es6",
+    details: "Timply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged"
   },
   {
-    title: "This is the first blog",
-    details: "this is the first blog i will ever make in my life, this is so awesome i don't think i am gonna stop making it"
+    title: "JavaScript Object Prototypes",
+    details: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to"
   },
   {
-    title: "This is the first blog",
-    details: "this is the first blog i will ever make in my life, this is so awesome i don't think i am gonna stop making it"
+    title: "Regular Expressions",
+    details: "this is the first blog i will ever make in my life, this is so awesome i don't think i am gonna stop making its simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged"
   },
   {
-    title: "This is the first blog",
+    title: "Neat CSS HACKS",
     details: "this is the first blog i will ever make in my life, this is so awesome i don't think i am gonna stop making it"
   }
 ];
