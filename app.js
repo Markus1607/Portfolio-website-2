34ea5342daa918c8443f5f0699d15d3213b31b5e
Mark Sarpong <markus20@outlook.com>
2017-06-05 19:30:55 +0100

    modified npm start

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   indentedSyntax: true, // true = .sass and false = .scss
-  outputStyle: "expanded",
+  outputStyle: "compressed",
   sourceMap: false,
   force: true
 }));
